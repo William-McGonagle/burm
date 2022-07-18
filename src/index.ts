d3bb35c3fe5cb69e63ec221588c516480d25b0eb
William McGonagle <wpmcgonagle@gmail.com>
2022-07-17 23:08:21 -0400

    Cleaned Up Index

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,6 @@ import DataType from "./dataType";
 import QueryManager from "./queryManager";
 import Condition from "./condition";
 
-// Allowed Databases
-// - MongoDB
-// - Sqlite
-// - SQL
-// - PostgreSQL
-// - Casandra
-
-const User = QueryManager.register('User', {
-    firstname: DataType.TEXT,
-    lastname: DataType.TEXT
-});
-
 export default QueryManager;
 export {
     DataType,
