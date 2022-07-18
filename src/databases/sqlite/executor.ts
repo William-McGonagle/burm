06354a344634ad96e7c79002659c3d19aac7334d
William McGonagle <wpmcgonagle@gmail.com>
2022-07-17 23:08:00 -0400

    Linked ORM to SQLite

--- src/databases/sqlite/executor.ts
+++ src/databases/sqlite/executor.ts
@@ -1,5 +1,10 @@
+import { Database } from "bun:sqlite";
+
+const db = new Database(":memory:");
+
 export default function executeDatabaseQuery(queryText) {
 
     console.log(queryText);
+    console.log(db.query(queryText).all());
 
 }
--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,6 @@ const User = QueryManager.register('User', {
     lastname: DataType.TEXT
 });
 
-let userCreateQuery = User.create({
-    firstname: "William",
-    lastname: "McGonagle"
-});
-
-let userSearchQuery = User.findAll({
-    where: Condition.and(
-        Condition.equals("firstname", "William"),
-        Condition.equals("lastname", "McGonagle")
-    )
-});
-
 export default QueryManager;
 export {
     DataType,
