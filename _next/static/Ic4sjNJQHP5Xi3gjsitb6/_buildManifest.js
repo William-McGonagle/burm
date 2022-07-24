8570e91314c98e282edacd370b633b9695fc3994
William-McGonagle <wpmcgonagle@gmail.com>
2022-07-24 16:01:24 +0000

    Deploying to gh-pages from @ William-McGonagle/burm@a2b281bacf901f1fb6622fcee3ac07765a3e799a 🚀

---
+++ _next/static/Ic4sjNJQHP5Xi3gjsitb6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-409eb6c542d48680.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
