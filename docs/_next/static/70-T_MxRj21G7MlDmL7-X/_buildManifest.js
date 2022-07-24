7e5caf55ded95432545c6b6365504d800d1989ae
William McGonagle <wpmcgonagle@gmail.com>
2022-07-24 11:46:31 -0400

    Purge the Docs

--- docs/_next/static/70-T_MxRj21G7MlDmL7-X/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5b32e7fcd4d9bfb6.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
